476987d0d13a037fd0af8f65c8ef06b8b6909ee8
Zahurul Islam <zahurulislam803@gmail.com>
2023-10-19 22:19:01 +0600

    integrate add to cart section and show success alert

--- src/Pages/ProductDetails.jsx
+++ src/Pages/ProductDetails.jsx
@@ -1,11 +1,48 @@
 import { useEffect, useState } from "react";
 import { useLoaderData, useParams } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const ProductDetails = () => {
   const {id} = useParams();
   const [products, setProducts] = useState({})
   const loadedData = useLoaderData();
-  
+
+  const productId = products._id;
+  const image = products.image;
+  const brand = products.brand;
+  const name = products.name;
+  const price = products.price;
+
+  const insertToCart = {
+    productId,
+    image,
+    brand,
+    name,
+    price,
+  };
+
+  const handleToCart = () => {
+    fetch("http://localhost:5000/cart", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(insertToCart),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.acknowledged) {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Product add to cart successfully",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
+
   useEffect(()=>{
     const findProduct = loadedData?.find(product => product._id == id);
     setProducts(findProduct);
@@ -27,7 +64,7 @@ const ProductDetails = () => {
         <h5>Rating: {products.rating}</h5>
         <p>Description: {products.desc}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-accent">Add To Cart</button>
+          <button onClick={handleToCart} className="btn btn-accent">Add To Cart</button>
         </div>
       </div>
     </div>
