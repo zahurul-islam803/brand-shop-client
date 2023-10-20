9633778bb86703c12dd8646ca1e3a312a8d6cc57
Zahurul Islam <zahurulislam803@gmail.com>
2023-10-20 08:25:44 +0600

    integrate dark light theme toggle in the whole website

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,18 +1,19 @@
+
 import { Outlet } from "react-router-dom";
 import Navbar from "../Header/Navbar";
 import Footer from "../../Pages/Footer";
-
 const Layout = () => {
+
   return (
-    <div className="max-w-screen-2xl mx-auto my-8 px-2">
-      <div className="mb-3">
-        <Navbar></Navbar>
+      <div className="max-w-screen-2xl mx-auto my-8 px-2">
+        <div className="mb-3">
+          <Navbar></Navbar>
+        </div>
+        <Outlet></Outlet>
+        <div className="my-14">
+          <Footer></Footer>
+        </div>
       </div>
-      <Outlet></Outlet>
-      <div className="my-14">
-        <Footer></Footer>
-      </div>
-    </div>
   );
 };
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,24 +6,24 @@ import NewsLatter from "../Components/Header/NewsLatter";
 
 const Home = () => {
   return (
-    <div className="mb-5">
-      <div>
-        <Header></Header>
-      </div>
-      <div className="mt-24 mb-10">
-        <h1 className="text-center text-6xl mb-10">Our Brands</h1>
+      <div className="mb-5">
         <div>
-          <Brands></Brands>
+          <Header></Header>
+        </div>
+        <div className="mt-24 mb-10">
+          <h1 className="text-center text-6xl mb-10">Our Brands</h1>
+          <div>
+            <Brands></Brands>
+          </div>
+        </div>
+        <div className="mt-24 mb-10">
+          <h1 className="text-center text-6xl mb-10">Latest Release</h1>
+          <LatestRelease></LatestRelease>
+        </div>
+        <div className="mt-24 mb-10">
+          <NewsLatter></NewsLatter>
         </div>
       </div>
-      <div className="mt-24 mb-10">
-        <h1 className="text-center text-6xl mb-10">Latest Release</h1>
-        <LatestRelease></LatestRelease>
-      </div>
-      <div className="mt-24 mb-10">
-        <NewsLatter></NewsLatter>
-      </div>
-    </div>
   );
 };
 
